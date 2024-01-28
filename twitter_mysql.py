d0ee321f051cfee15d8de2c372c595243714425e
Ceara Zhang <cearazhang@Cearas-MacBook-Pro.local>
2024-01-27 21:23:56 -0500

    initial files, beginning to implement redis into mysql.py

---
+++ twitter_mysql.py
@@ -0,0 +1,181 @@
+"""
+Julia Geller and Ceara Zhang
+DS4300 / Twitter Relational Database
+Created 14 Jan 2024
+Updated: 21 Jan 2024
+
+twitter_mysql.py:
+Twitter Database API for MySQL
+"""
+
+from dbutils import DBUtils
+from twitter_objects import Tweet, Follows
+from typing import List
+import random
+import pandas as pd
+import redis
+
+
+class TwitterAPI:
+
+    def __init__(self, host="localhost", port=6379, db=0):
+
+        # Connect to redis
+        self.redis_db = redis.StrictRedis(host=host, port=port, db=db)
+
+    # def setup_database(self):
+    #     """
+    #     Sets up the twitter database with its appropriate tables and schema.
+    #     """
+    #     # switch to the twitter database
+    #     use_db_query = """
+    #     USE twitter
+    #     """
+    #     self.dbu.execute(use_db_query)
+    #
+    #     # create the tweet table
+    #     create_tweet_table_query = """
+    #     CREATE TABLE tweet (
+    #         user_id INT,
+    #         tweet_ts DATETIME,
+    #         tweet_text VARCHAR(140),
+    #         INDEX idx_user_id (user_id),  -- additional index on user_id
+    #         INDEX idx_tweet_ts (tweet_ts)
+    #     )
+    #     """
+    #     self.dbu.execute(create_tweet_table_query)
+    #
+    #     # create the follows table
+    #     create_follows_table_query = """
+    #     CREATE TABLE follows (
+    #         user_id INT,
+    #         follows_id INT,
+    #         PRIMARY KEY(user_id, follows_id),
+    #         INDEX idx_user_id (user_id), -- additional index on user_id
+    #         INDEX idx_follows_id (follows_id) -- additional index on follows_id
+    #     )
+    #     """
+    #     self.dbu.execute(create_follows_table_query)
+
+    def destroy_database(self):
+        """
+        Destroys the twitter database (for testing purposes).
+        """
+        # destroy the twitter database
+        self.redis_db.flushall()
+
+    def post_tweet(self, twt: Tweet):
+        """
+        UUse tweet ID as key and store tweet data as value
+
+        Args:
+        tweet: Tweet
+        """
+        tweet_key = f"Tweet:{twt.tweet_id}"
+        tweet_data = {
+            
+        }
+
+        sql = "INSERT INTO tweet (user_id, tweet_text, tweet_ts) VALUES (%s, %s, %s) "
+        val = (twt.user_id, twt.tweet_text, twt.tweet_ts)
+        self.dbu.insert_one(sql, val)
+
+    def register_follows(self, fol: Follows):
+        """
+        Register a follow into the follows table.
+
+        Args:
+        fol: Follows
+        """
+        sql = "INSERT INTO follows (user_id, follows_id) VALUES (%s, %s) "
+        val = (fol.user_id, fol.follows_id)
+        self.dbu.insert_one(sql, val)
+
+    def choose_rand_user(self) -> int:
+        """
+        Returns a random user from the follows table.
+
+        Args:
+
+        return:
+        user_id: int
+        """
+        sql = """
+            SELECT user_id
+            FROM follows
+            """
+        user_ids = self.dbu.execute(sql)['user_id'].values
+        user_id = random.choice(user_ids)
+        return user_id
+
+    def get_follows(self, user_id: int) -> List[int]:
+        """
+        Return the ids of each unique account that the given user follows.
+
+        Args:
+        user_id: int
+
+        return:
+        follows_ids: List[int]
+        """
+        sql = f"""
+            SELECT follows_id
+            FROM follows
+            WHERE user_id = {user_id}"""
+        follows_ids = list(set(self.dbu.execute(sql)['follows_id'].values))
+        return follows_ids
+
+    def get_tweets(self, user_ids: List[int]) -> pd.DataFrame:
+        """
+        Returns every tweet posted and the time it was posted
+        for all tweets posted by the given user.
+
+        Args:
+        user_ids: List[int]
+
+        return:
+        result: pd.DataFrame
+        """
+        user_ids = '(' + ', '.join([str(id) for id in user_ids]) + ')'
+        sql = f"""
+            SELECT tweet_text, tweet_ts
+            FROM tweet t
+            WHERE user_id in {user_ids}
+            ORDER BY t.tweet_ts DESC
+            LIMIT 10;
+            """
+        result = self.dbu.execute(sql)
+        return result
+
+    def get_home_timeline(self, user_id: int) -> pd.DataFrame:
+        """
+        Retrieves the home timeline of the given user,
+        query 10 most recent tweets from users they follow.
+
+        Args:
+        user_id: int
+
+        return:
+        result: pd.DataFrame
+        """
+        follows_ids = self.get_follows(user_id)
+        result = self.get_tweets(follows_ids)
+        return result
+
+    def simulate_home_timeline_refresh(self, num_refreshes: int, verbose=False):
+        """
+        Simulate the home timeline refresh by selecting a random
+        user each time to retrieve their home timeline of the 10 most recent tweets.
+
+        Args:
+        num_refreshes: int
+        verbose: bool
+        """
+        for _ in range(num_refreshes):
+            # pick a random user
+            random_user_id = self.choose_rand_user()
+
+            # retrieve and print home timeline for the random user
+            home_timeline = self.get_home_timeline(random_user_id)
+            if verbose:
+                print(f"Home timeline for user {random_user_id}: {home_timeline}")
---
+++ twitter_tester.py
@@ -0,0 +1,77 @@
+"""
+Julia Geller and Ceara Zhang
+DS4300 / Twitter Relational Database
+Created 14 Jan 2024
+Updated: 21 Jan 2024
+
+twitter_tester.py:
+Driver that uses Twitter API to post tweets and
+retrieve user home timelines.
+"""
+
+import os
+import pandas as pd
+from twitter_mysql import TwitterAPI
+from twitter_objects import Tweet, Follows
+import time
+from datetime import datetime
+
+
+def main():
+    # create user and password environment variables, must be done
+    # upon re-running the program as these variables do not persist
+    os.environ["TWITTER_USER"] = 'root'
+    os.environ["TWITTER_PASSWORD"] = 'cece2002'  # TODO fill in with your password
+
+    # save tweets and follows data as dataframes
+    tweets_df = pd.read_csv('../data/tweet.csv')
+    follows_df = pd.read_csv('../data/follows.csv')
+
+    # connect to twitter database with this user
+    api = TwitterAPI(os.environ["TWITTER_USER"], os.environ["TWITTER_PASSWORD"], "twitter")
+
+    # set up the database with its appropriate schema
+    api.setup_database()
+
+    # start a one-second timer
+    sec_timer = time.time()
+    # save a counter of how many tweets have been posted in one second
+    twts_per_sec = 0
+
+    # insert the tweet rows into the table (i.e. post the tweets)
+    for _, row in tweets_df.iterrows():
+        # format the current time as the tweet's time posted
+        tweet_ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # create the Tweet object
+        tweet = Tweet(row['USER_ID'], row['TWEET_TEXT'], tweet_ts)
+        # post the tweet
+        api.post_tweet(tweet)
+        # increment the number of tweets posted if the second timer is not up
+        if time.time() - sec_timer <= 1:
+            twts_per_sec += 1
+    print(f'{twts_per_sec} tweets were posted in one second.')
+
+    # insert the follows rows into the table (i.e. register the tweets)
+    for _, row in follows_df.iterrows():
+        fols = Follows(row['USER_ID'], row['FOLLOWS_ID'])
+        # register the follows
+        api.register_follows(fols)
+
+    # start a one-second timer
+    sec_timer = time.time()
+    # save a counter of how many home timelines are retrieved per second
+    ht_per_sec = 0
+    while time.time() - sec_timer < 1:
+        # retrieve one user's home timeline
+        api.simulate_home_timeline_refresh(1)
+        # increment counter
+        ht_per_sec += 1
+    # print number of home timelines retrieved per second
+    print(f'{ht_per_sec} home timelines were retrieved in one second.')
+
+    # destroy database when done (for re-running/testing purposes)
+    api.destroy_database()
+
+
+if __name__ == '__main__':
+    main()
